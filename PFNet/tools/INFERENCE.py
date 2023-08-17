2c449c33b4a35890b709c6d28edc74f884bde1e4
integralSF <2021132070@stu.cuit.edu.cn>
2023-08-17 09:32:59 +0800

    add inference noramlize, and put code in order

--- PFNet/tools/INFERENCE.py
+++ PFNet/tools/INFERENCE.py
@@ -3,6 +3,7 @@
 import numpy as np
 import argparse
 import time
+import os
 import torch
 import torch.nn.parallel
 import torch.utils.data
@@ -11,18 +12,23 @@ sys.path.append("/home/zjh/workspace/workspace-git/PFNet/")
 import utils
 from utils import PointLoss
 from model_PFNet import _netG
+import NORMALIZE
 
 
 # ----------PARSER FOR INFERENCE-------------
-model_num = '9'
-model_iter = '350'
-data_num = '101-42' # 60-24 62-26 140-45(MOYA)
+model_num = '10'
+model_iter = '500'
+data_num = '99-27' # 60-24 62-26 140-45(MOYA)
                     # 66-13 95-23 101-42(JIAYA)
+
+tooth_type = "MOYA"
 
 parser = argparse.ArgumentParser()
 # ----------------------------------------INPUT FILE PATH-------------------------------------------------------
-parser.add_argument('--infile',type = str, default = f'./test_one(JIAYA)/partial-normalize-{data_num}.txt')
-parser.add_argument('--infile_real',type = str, default = f'./test_one(JIAYA)/gt-normalize-{data_num}.txt')
+parser.add_argument('--infile_real',type = str, default =
+                    f'dataset/test_dataset_processed/val/gt/4-molars/molars-gt-99-27.pts')
+parser.add_argument('--infile',type = str, default = 
+                    f'dataset/test_dataset_processed/val/partial/4-molars/molars-partial-99-27.pts')
 parser.add_argument('--netG', default=f'./checkpoint/Trained_Model_{model_num}/point_netG{model_iter}.pth', help="path to netG (to continue training)")
 
 
@@ -50,6 +56,9 @@ point_netG.eval()
 
 # ---------------------READ DATA AND CROP--------------------------------------
 input_cropped1 = np.loadtxt(opt.infile,delimiter=' ')
+
+input_cropped1 = NORMALIZE.pc_normalize(input_cropped1)
+
 input_cropped1 = torch.FloatTensor(input_cropped1)
 input_cropped1 = torch.unsqueeze(input_cropped1, 0)
 input_cropped2_idx = utils.farthest_point_sample(input_cropped1,opt.point_scales_list[1],RAN = True)
@@ -63,8 +72,10 @@ input_cropped  = [input_cropped1,input_cropped2,input_cropped3]
 
 
 # -------------------INPUT AND INFERENCE--------------------
+time1 = time.time()
 fake_center1,fake_center2,fake=point_netG(input_cropped)
-
+print("-------------------------")
+print("INFERENCE TIME: %.4f"%(time.time() - time1))
 
 # ---------PROCESS FAKE----------
 fake =fake.cpu()
@@ -73,6 +84,9 @@ np_fake = fake[0].detach().numpy()
 
 # --------------REAL GT DATA-------------------------
 real = np.loadtxt(opt.infile_real,delimiter=' ')
+
+real = NORMALIZE.pc_normalize(real)
+
 real = torch.FloatTensor(real)
 real = torch.unsqueeze(real,0)
 # if want see other sclar
@@ -81,11 +95,18 @@ real = torch.unsqueeze(real,0)
 # real3_idx = utils.farthest_point_sample(real,256, RAN = True)
 # real3 = utils.index_points(real,real3_idx)
 real = real.cpu()
+np_real = real[0].detach().numpy()
 
 
 # ------------------------CALU THE CD LOSS----------------------------------
 CD_LOSS = criterion_PointLoss(torch.squeeze(fake,1),torch.squeeze(real,1))
+print("CD_LOSS: %.4f" % (CD_LOSS.item()),
+      "\n-------------------------")
 
+
+folder_path = f'test_one({tooth_type})/{model_num}'
+os.makedirs(folder_path, exist_ok=True)
 
 # --------------------------SVAE RESULT-------------------------------------
-np.savetxt(f'test_one(JIAYA)/{model_num}/{model_num}_{data_num}_test_output_normalize_fake'+'.txt', np_fake, fmt = "%f %f %f")
+np.savetxt(f'test_one({tooth_type})/{model_num}/{model_num}_{model_iter}_{data_num}_test_output_normalize_fake'+'.txt', np_fake, fmt = "%f %f %f")
+np.savetxt(f'test_one({tooth_type})/{model_num}/{model_num}_{model_iter}_{data_num}_test_output_normalize_real'+'.txt', np_real, fmt = "%f %f %f")
